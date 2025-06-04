da15fa41cbe7ee2655b7e8996ce367914fb05683
fathanNova <novaaroyan13@gmail.com>
2025-06-04 22:53:55 +0700

    fix typo

--- FinalProject.cpp
+++ FinalProject.cpp
@@ -21,19 +21,20 @@ void show();
 void push(); //fungsi menampilkan barang baru
 void searchByName();
 void searchById();
+
 int main() {
     init();
     int choice; // untuk menampilkan menu pilihan
     
     do{
-      cout << " \n-----Menu-----\n";
-      cout << "1. Tambahkan Baarang\n";
+      system("cls");
+      cout << " \n-----Program Pendataan Barang Gudang-----\n\n";
+      cout << "1. Tambahkan Barang\n";
       cout << "2. Tampilkan Barang\n";
       cout << "3. Cari Barang Berdasarkan Nama\n";
       cout << "4. Cari Barang Berdasarkan ID\n";
-      cout << "5. Keluar\n";
-      cout << "Pilih Opsi lain : ";
-      cin >> choice;
+      cout << "5. Keluar\n\n";
+      cout << "Masukkan Pilihan Anda : "; cin >> choice;
         switch (choice) {
             case 1:
             push();
@@ -48,13 +49,14 @@ int main() {
             searchById();
             break;
             case 5:
-            cout <<" Keluar dari program.\n";
+            cout <<"Keluar dari program.\n\n";
             break;
             default:
             cout << "Pilihan tidak valid. Silahkan coba lagi./n";
-    }
+        }
     }while(choice != 5);
-        return 0;
+    system("pause");
+    return 0;
 }
 
 void init(){    //Satria
@@ -85,57 +87,53 @@ void show() {
 
 void push() {
     if (!full()) {
-cout << "Masukkan ID Barang: ";
-cin >> barang.id;
-cout << "Masukkan Nama Barang:";
-cin >> barang.nama;
-cout << "Masukkan Jumlah Barang:";
-cin >> barang.jumlah;
-stack.data[++stack.top] = barang;
-}else {
-cout <<"Stack sudah penuh.\n";
+        cout << "Masukkan ID Barang: "; cin >> barang.id;
+        cout << "Masukkan Nama Barang:"; cin >> barang.nama;
+        cout << "Masukkan Jumlah Barang:"; cin >> barang.jumlah;
+        stack.data[++stack.top] = barang;
+    }else {
+        cout <<"Stack sudah penuh.\n";
     }
 }
 
 void searchByName() {
     string name;
-cout << " Masukkan Nama Barang yang dicari: ";
-cin >> name;
-bool found = false;
+    cout << " Masukkan Nama Barang yang dicari: ";
+    cin >> name;
+    bool found = false;
 
-for (int i = 0; i <= stack.top; i++) {
-if (stack.data [i].nama == name) {
-cout << "Barang ditemukan:\n";
-cout << " ID      : " << stack.data[i].id<< endl;
-cout << "Nama     : " << stack.data[i].nama<< endl;
-cout << "Jumlah   : " << stack.data[i].jumlah << endl;
-found = true;
-break;
-}
-}
-
-if (!found) {
-cout << " Barang dengan nama '"<< name << "' tidak ditemukan.\n";
-}
+    for (int i = 0; i <= stack.top; i++) {
+        if (stack.data [i].nama == name) {
+            cout << "Barang ditemukan:\n";
+            cout << " ID      : " << stack.data[i].id<< endl;
+            cout << "Nama     : " << stack.data[i].nama<< endl;
+            cout << "Jumlah   : " << stack.data[i].jumlah << endl;
+            found = true;
+            break;
+        }
+    }
+    if (found == false ) {
+        cout << " Barang dengan nama '"<< name << "' tidak ditemukan.\n";
+    }
 }
 
 void searchById() {
     int id;
-cout << " Masukkan ID barang yang dicari: ";
-cin >> id;
-bool found = false;
+    cout << " Masukkan ID barang yang dicari: ";
+    cin >> id;
+    bool found = false;
 
-for (int i = 0; i <= stack.top; i++) {
-if (stack.data[i].id == id) {
-cout << "Barang ditemukkan:\n";
-cout  << " ID    : " << stack.data[i].id <<endl;
-cout << " Nama   : " << stack.data[i].nama <<endl;
-cout << "Jumlah : " << stack.data[i].jumlah << endl;
-found = true;
-break;
-}
-}
-if (!found) { 
-cout << " Barang dengan ID '" << id << "' tidak ditemukan.\n";
-}
+    for (int i = 0; i <= stack.top; i++) {
+        if (stack.data[i].id == id) {
+            cout << "Barang ditemukkan:\n";
+            cout  << " ID    : " << stack.data[i].id <<endl;
+            cout << " Nama   : " << stack.data[i].nama <<endl;
+            cout << "Jumlah : " << stack.data[i].jumlah << endl;
+            found = true;
+            break;
+        }
+    }
+    if (!found) { 
+        cout << " Barang dengan ID '" << id << "' tidak ditemukan.\n";
+    }
 }
